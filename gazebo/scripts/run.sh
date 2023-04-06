6c02023bb71f2ef045f413aec76c577663054ce2
Chris Choi <chutsu@gmail.com>
2023-04-06 16:25:46 +0100

    Remove gazebo/scripts

--- gazebo/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# export GZ_SIM_SERVER_CONFIG_PATH=$PWD/server.config
-export GZ_SIM_RESOURCE_PATH=$PWD/worlds:$PWD/models
-export GZ_SIM_SYSTEM_PLUGIN_PATH=$PWD/build
-mkdir -p build && cd build && cmake .. && make
-gz sim sandbox.sdf

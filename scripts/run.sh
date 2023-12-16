7f54ce2a5c8ce029c01caad0a526688931b25264
Chris Choi <chutsu@gmail.com>
2023-12-16 16:51:13 +0000

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,50 +1,9 @@
 #!/bin/bash
 set -e
 
-# { CMD=$(cat); } << EOF
-# . /opt/ros/humble/setup.sh \
-#   && cd $HOME/projects/proto_ros2 \
-#   && make build && ros2 run proto_ros2 oak_stereo_imu_node.py
-# EOF
-# tmux send-keys -t dev -R C-l C-m
-# tmux send-keys -t dev -R "$CMD" C-m C-m
-
-# { CMD=$(cat); } << EOF
-# . /opt/ros/humble/setup.sh \
-#   && cd /home/docker/proto_ros2 \
-#   && make sim_sandbox
-# EOF
-# tmux send-keys -t docker -R C-l C-m
-# tmux send-keys -t docker -R "$CMD" C-m C-m
-
-# { CMD=$(cat); } << EOF
-# . /opt/ros/humble/setup.sh \
-#   && cd /home/docker/proto_ros2 \
-#   && make build_docker
-# EOF
-# tmux send-keys -t docker -R C-l C-m
-# tmux send-keys -t docker -R "$CMD" C-m C-m
-
-# rm -rf /home/chutsu/record
-# python3 src/oak_stereo_imu_node.py --output_path /home/chutsu/record
-# CMD="python3 src/oak_stereo_imu_node.py --output_path /home/chutsu/record"
-# # CMD="python3 src/oak_stereo_imu_record.py"
-# CMD="python3 src/oak_imu_node.py"
-# tmux send-keys -t dev -R C-l C-m
-# tmux send-keys -t dev -R "cd ~/projects/proto_ros2 && $CMD" C-m C-m
-
-
 # tmux send-keys -t dev -R C-l C-m
 # tmux send-keys -t dev -R "\
 # cd ~/proto_ws && colcon build && source $HOME/proto_ws/install/setup.bash && ros2 run proto_ros2 sbgc_node
-# " C-m C-m
-
-# tmux send-keys -t dev -R C-l C-m
-# tmux send-keys -t dev -R "\
-# cd ~/proto_ws \
-#   && colcon build \
-#   && source install/setup.bash  \
-#   && ros2 run proto_ros2 rs_node
 # " C-m C-m
 
 # tmux send-keys -t dev -R C-l C-m
@@ -61,20 +20,17 @@ set -e
 #   && cd ~/proto_ws \
 #   && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release \
 #   && source install/setup.bash \
-#   && ros2 run proto_ros2 calib_gimbal
+#   && ros2 run proto_ros2 calib_gimbal_inspect
 # " C-m C-m
 
-tmux send-keys -t dev -R C-l C-m
-tmux send-keys -t dev -R "\
-  rm -rf ~/calib_gimbal_data \
-  && cd ~/proto_ws \
-  && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release \
-  && source install/setup.bash \
-  && ros2 run proto_ros2 calib_camimu_record
-" C-m C-m
-
 # tmux send-keys -t dev -R C-l C-m
-# tmux send-keys -t dev -R "cd ~/projects/proto_ros2 && python3 scripts/calib_gimbal.py" C-m C-m
+# tmux send-keys -t dev -R "\
+#   rm -rf ~/calib_gimbal_data \
+#   && cd ~/proto_ws \
+#   && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release \
+#   && source install/setup.bash \
+#   && ros2 run proto_ros2 calib_camimu_record
+# " C-m C-m
 
 # tmux send-keys -t dev -R C-l C-m
 # tmux send-keys -t dev -R "\
@@ -86,3 +42,22 @@ tmux send-keys -t dev -R "\
 #     -p config_path:=/home/chutsu/config_realsense_D435i_Chris.yaml \
 #     -p dbow_dir:=/home/chutsu/projects/okvis2
 # " C-m C-m
+
+# tmux send-keys -t dev -R C-l C-m
+# tmux send-keys -t dev -R "\
+#   cd ~/proto_ws \
+#   && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release \
+#   && source install/setup.bash \
+#   && ros2 run proto_ros2 rs_node
+# " C-m C-m
+
+tmux send-keys -t dev -R C-l C-m
+tmux send-keys -t dev -R "\
+cd ~/proto_ws \
+  && colcon build --packages-select proto_ros2 \
+  && colcon build --packages-select ros2_vicon \
+  && source install/setup.bash \
+  && ros2 launch proto_ros2 vicon_experiments.py
+" C-m C-m
+
+# python3 scripts/okvis_calib_converter.py

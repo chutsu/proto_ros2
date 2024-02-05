cc4ac1f8f78f6d87ebc2f7dee4277b96c2b91acc
Chris Choi <chutsu@gmail.com>
2024-02-05 16:12:03 +0000

    Update vicon_experiments.launch.py

--- launch/vicon_experiments.launch.py
+++ launch/vicon_experiments.launch.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
+import os
+
 import launch
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-  rs_node = Node(package="proto_ros2", executable="rs_node", name="rs_node")
+  script_dir = os.path.dirname(__file__)
+  config_dir = os.path.abspath(os.path.join(script_dir, "../config"))
+
+  config_path = f"{config_dir}/realsense_d435i.yaml"
+  okvis_params = [{"config_path": config_path}]
+  okvis_node = Node(package="okvis",
+                    executable="okvis_gimbal_node",
+                    name="okvis_gimbal_node",
+                    parameters=okvis_params,
+                    # prefix=["gdb -ex=r -ex bt --args"],
+                    output="screen")
+
+  sbgc_node = Node(package="proto_ros2",
+                   executable="sbgc_node",
+                   name="sbgc_node")
+
   vicon_node = Node(package="ros2_vicon",
                     executable="vicon_node",
                     name="vicon_node",
@@ -14,4 +32,4 @@ def generate_launch_description():
                             "hostname": "10.0.5.127"
                         },
                     ])
-  return launch.LaunchDescription([rs_node, vicon_node])
+  return launch.LaunchDescription([okvis_node, sbgc_node, vicon_node])
